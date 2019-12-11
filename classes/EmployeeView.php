d2981842fd9f4f6f0b7bac820de2b6e380c7deac
Jagoda <jagoda.kosinska87@gmail.com>
2019-12-11 15:04:33 +0100

    change db connect, model Employee->get_employees & EmployeeView->showEmployees

--- classes/EmployeeView.php
+++ classes/EmployeeView.php
@@ -5,7 +5,6 @@ class EmployeeView extends Employee{
 
     public function showEmployees(){
     $result = $this->get_employees();
-    // var_dump($result);
     echo "First Name:" . $result[0]['fname'];
     }
 }
