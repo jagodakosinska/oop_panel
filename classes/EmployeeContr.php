4c89168ea9e73d7bc2ad73365af8538b94c812b7
Jagoda <jagoda.kosinska87@gmail.com>
2019-12-12 11:26:16 +0100

    add to EmployeeContr method ereate_employee

--- classes/EmployeeContr.php
+++ classes/EmployeeContr.php
@@ -63,5 +63,62 @@ class EmployeeContr extends Employee
         $this->displayer->load_view($data, $template_name);
     }
 
+    public function create_employee()
+    {
+        $arr = $this->validation_data();
+        if ($arr['status'] === true) {
+            $id = $this->set_employee($arr);
+            $this->show_employee($id);
+        } else {
+            $this->p['errors'] = $arr['data'];
+            $this->add_new_employee();
+        }
+    }
+
+
+
+    // $list_employee = empty($p);
+    // $show_employee = isset($p['show_emp']) && is_numeric($p['show_emp']);
+    // $edit_employee = isset($p['edit_emp']) && is_numeric($p['edit_emp']);
+    // $update_employee = isset($p['update_emp']) && isset($p['id']) && is_numeric($p['id']);
+    // $add_new_employee = isset($p['add_emp']);
+    // $insert_employee = isset($p['insert_emp']) && $p['insert_emp'] === 'Dodaj';
+
+    // without view
+
+    // if ($update_employee) {
+    //     $id = $p['id'];
+    //     $arr = $valid->valid_employee($p['emp']);
+    //     if ($arr['status'] === true) {
+    //         $emp->update_emp($id, $arr['data']);
+    //         $show_employee = true;
+    //     $p['show_emp'] = $p['id'];
+    //     }else{
+    //         $p['edit_emp'] = $p['id'];
+    //         $p['errors'] = $arr['data'];
+    //         $edit_employee = true;
+    //     }
+    // ================   Employee 
+    // if ($edit_employee) {
+    //     $template_name = 'views/employee/form.php';
+    //     $data['page_title'] = "Edycja Pracownika";
+    //     $id = $p['edit_emp'];
+    //     $data['submit']  = 'update_emp';
+    //     $data['value'] = 'Edytuj';
+    //     $data['errors'] = $p['errors'];
+    //     // $data['emp'] = ;
+    //     $data['emp'] = isset($p['emp']) && empty($p['emp']) ? $p['emp'] :  $emp->get_by_id($id)[0];
+    //     $displayer->load_view($data, $template_name);
+    // } elseif ($add_new_employee) {
+    //     $template_name = 'views/employee/form.php';
+    //     $data['page_title'] = "Dodaj Pracownika";
+    //     $data['emp'] = '';
+    //     $data['submit'] = 'insert_emp';
+    //     $data['value'] = 'Dodaj';
+    //     $data['errors'] = $p['errors'];
+    //     $data['emp'] = isset($p['emp']) && empty($p['emp']) ? $p['emp'] : $data['emp'];
+    //     $displayer->load_view($data, $template_name);
+    // }
+
 
 }
