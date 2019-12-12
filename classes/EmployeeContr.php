3775000bfa97edb181663ecf49fffc230c086584
Jagoda <jagoda.kosinska87@gmail.com>
2019-12-12 11:23:26 +0100

    add to EmployeeContr method Validator & Displayer

--- classes/EmployeeContr.php
+++ classes/EmployeeContr.php
@@ -8,4 +8,28 @@ class EmployeeContr extends Employee
     var $valid;
     var $p;
     
+
+    public function __construct()
+    {
+        parent::__construct();
+        // include 'autoloader.php';
+        $this->empView = new EmployeeView();
+        $this->displayer = new Displayer();
+        $this->valid = new Validator();
+        $this->p = array_merge($_POST, $_GET);
+        // $this->p['errors'] = [];
+    }
+
+    public function validation_data()
+    {
+
+        $arr = $this->valid->valid_employee($this->p['emp']);
+        return $arr;
+    }
+
+    public function displayer($data, $template_name)
+    {
+        $this->displayer->load_view($data, $template_name);
+    }
+
 }
