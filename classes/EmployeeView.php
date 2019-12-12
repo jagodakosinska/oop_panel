3ba34aa99e439bfc5b7afe4c7357d49ef586c3de
Jagoda <jagoda.kosinska87@gmail.com>
2019-12-12 11:27:40 +0100

    return EmployeeView method showEmployees

--- classes/EmployeeView.php
+++ classes/EmployeeView.php
@@ -5,6 +5,6 @@ class EmployeeView extends Employee{
 
     public function showEmployees(){
     $result = $this->get_employees();
-    echo "First Name:" . $result[0]['fname'];
+    return $result;
     }
 }
