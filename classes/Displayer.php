b2a773473c065fd302b1dd4b8f5df6e7d683054d
Jagoda <jagoda.kosinska87@gmail.com>
2019-12-12 13:29:01 +0100

    extend method check_form in Displayer class

--- classes/Displayer.php
+++ classes/Displayer.php
@@ -13,8 +13,11 @@ class Displayer{
     }
 
     function check_form($key, $arr) {
-    if(is_array($arr)) {
-        return isset($arr[$key]) ? $arr[$key] : '';
-    }
+        if(is_array($arr)) {
+            return isset($arr[$key]) ? $arr[$key] : '';
+        
+        } elseif(is_object($arr)) {
+            return isset($arr->{$key}) ? $arr->{$key} : '';
+        }
     }
 }
